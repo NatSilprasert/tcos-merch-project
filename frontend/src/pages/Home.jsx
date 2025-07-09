e9aecf3457cd979567e080cf90dfb0b87cb328e9
Nat Silprasert <natsilprasert@gmail.com>
2025-07-09 11:29:10 +0700

    Fix

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,34 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState, useSyncExternalStore } from 'react'
 import Hero from '../components/Hero'
 import MerchSection from '../components/MerchSection'
 import { ShopContext } from '../context/ShopContext';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const Home = () => {
 
-  const { products } = useContext(ShopContext);
+  const { token, backendUrl } = useContext(ShopContext);
+  const [ products, setProducts ] = useState([]);
+
+  const fetchProductData = async () => {
+    try {
+            
+      const response = await axios.get(backendUrl + '/api/product/list')
+      if (response.data.success) {
+          setProducts(response.data.products)
+      } else {
+          toast.error(response.data.message)
+      }
+
+  } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+  }
+
+  }
+  useEffect(() => {
+    fetchProductData();
+  }, [token])
 
   return (
     <div>
